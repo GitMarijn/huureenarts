4018f6abc85fa35c03092ff1ac8c27055267cd58
Martijn Beenker <martijn.beenker@avanade.com>
2020-08-23 16:23:28 +0200

    Add connection string for db

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://artshuren:kedbid-vatsaB-cozjy4@cluster0-gatgd.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb://wouter-ac:5Ru31CHl4BpDpHJTk1k2Yw1QPpwjMKwwzEQvnNgWYudwwY8dmswBfVlmeD89eUUtfR8p3zjBZtLffFNPzgokvQ==@wouter-ac.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@wouter-ac@"
 };
